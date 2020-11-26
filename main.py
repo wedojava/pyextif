cdd21f091ebfc74888fac65b287cf35e7ecedf3e
MyMac <wedojava@gmail.com>
2020-11-26 17:33:12 +0800

    humanized interactive input optimized.

--- main.py
+++ main.py
@@ -2,27 +2,22 @@ from controller import Controller
 from osgeo import gdal
 
 
-def envelope(src):
-    ds = gdal.Open(src)
-    metadata = dict()
-    ulx, xres, _, uly, _, yres = ds.GetGeoTransform()
-    lrx = ulx + (ds.RasterXSize * xres)  # low right x
-    lry = uly + (ds.RasterYSize * yres)  # low right y
-    metadata['minx'] = ulx
-    maxx = lrx  # ulx + xres * ds.RasterXSize
-    metadata['maxx'] = maxx
-    miny = lry  # uly + yres * ds.RasterYSize
-    metadata['miny'] = miny
-    metadata['maxy'] = uly
-    return metadata
-
-
-if __name__ == "__main__":
-    # a = envelope("./example/test/test.tif")
-    # print(a)
+def main():
+    src = input("Input tiff files dir(default is \".\"):")
+    cfg = input("Input config file path(default is \"./config.txt\"):")
     # c = Controller()
     # c = Controller("./example", "./example/config.txt")
-    c = Controller(cfg="./example/config.txt")
+    # c = Controller(cfg="./example/config.txt")
+    c = Controller(src, cfg)
     c.set_tifs_area()
     print(c.tifs[0].areanames)
     c.rename()
+
+if __name__ == "__main__":
+    while 1:
+        try:
+            main()
+        except FileNotFoundError:
+            print("[-] Please make sure config file at right spot as you input.")
+        except Exception as err:
+            print(err)
